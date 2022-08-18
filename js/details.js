d4a4cf5463dec8c5cc6f41382a379ae2b30931b7
Sadia88 <sadiasharmin460280@gmail.com>
2022-08-18 22:49:03 +0600

    correction

--- js/details.js
+++ js/details.js
@@ -3,7 +3,10 @@ let WAmount=0;
 // console.log('clicked')
 document.getElementById('btn-deposit').addEventListener('click',function(){
 let depositFiled=parseFloat(document.getElementById('deposit-filed').value)
-
+if(isNaN(depositFiled)){
+    alert('Type valid number')
+    return
+}
 depositeTotalMoney=depositFiled+depositeTotalMoney;
 
 
@@ -23,13 +26,25 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     
    
     let withdrawtFiled=parseFloat(document.getElementById('withdraw-filed').value)
+    if(isNaN(withdrawtFiled)){
+        alert('Type valid number')
+        return
+    }
+    const balanceAmount=document.getElementById('balance-amount')
+    TotalBalanceAmountString=balanceAmount.innerText
+    TotalBalanceAmount=parseFloat(TotalBalanceAmountString)   
+
+    if(withdrawtFiled >TotalBalanceAmount){
+        alert("You have no balance")
+    }
+   else{
     WAmount=withdrawtFiled+WAmount;
 
-   document.getElementById('w-amount').innerText=WAmount
+    document.getElementById('w-amount').innerText=WAmount
 
-   const balanceAmount=document.getElementById('balance-amount')
-TotalBalanceAmountString=balanceAmount.innerText
-TotalBalanceAmount=parseFloat(TotalBalanceAmountString)
-Balance=TotalBalanceAmount-withdrawtFiled;
-balanceAmount.innerText=Balance
+ 
+ Balance=TotalBalanceAmount-withdrawtFiled;
+ balanceAmount.innerText=Balance
+   }
+withdrawtFiled.value=''
 })
